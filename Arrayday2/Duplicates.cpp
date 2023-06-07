a2746ea74372e58a0533caf7626fd02bbeae09ba
karananu <karanrana3095@gmail.com>
2023-06-07 22:52:58 +0530

    codecommit

--- Arrayday2/Duplicates.cpp
+++ Arrayday2/Duplicates.cpp
@@ -1,33 +1,22 @@
-#include <iostream>
-using namespace std;
-
-void duplicate(int num[], int n)
-{
-    int sum = 0;
-    for (int i = 0; i < n; i++)
-    {
-        sum = sum + num[i];
+int low = 1, high = nums.size() - 1, cnt;
+        
+        while(low <=  high)
+        {
+            int mid = low + (high - low) / 2;
+            cnt = 0;
+            // cnt number less than equal to mid
+            for(int n : nums)
+            {
+                if(n <= mid)
+                    ++cnt;
+            }
+            // binary search on left
+            if(cnt <= mid)
+                low = mid + 1;
+            else
+            // binary search on right
+                high = mid - 1;
+            
+        }
+        return low;
     }
-    int sum2 = n * (n + 1) / 2;
-
-    int ans = sum - sum2;
-    cout << ans;
-}
-int main()
-{
-    int n;
-    cin >> n;
-    int num[100];
-    for (int i = 0; i < n; i++)
-    {
-        // input in array
-        cin >> num[i];
-    }
-    duplicate(num, n);
-    for (int i = 0; i < n; i++)
-    {
-        cout << num[i] << " ";
-    }
-
-    return 0;
-}
